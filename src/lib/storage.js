c184f4f7f15ae5baa25a7489880f065adac563ae
ali <nabiy5511@gmail.com>
2026-05-15 17:50:13 +0500

    Updated.

---
+++ src/lib/storage.js
@@ -0,0 +1,51 @@
+class Storage {
+  async clear(data) {
+    try {
+      return;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async save(file) {
+    try {
+      return;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async saveMany(files) {
+    try {
+      return;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async get(filename) {
+    try {
+      return;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async delete(filename) {
+    try {
+      return;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteMany(filenames) {
+    try {
+      return;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+module.exports = new Storage();
